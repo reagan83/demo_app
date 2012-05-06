600d6007eef0a00e01de382c09985989f83a5b0d
Reagan Williams <reagan.williams@gmail.com>
2012-05-05 22:59:26 -0700

    add title helper

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ DemoApp::Application.routes.draw do
   get "static_pages/help"
 
   get "static_pages/about"
+  get "static_pages/contact"
 
   resources :users
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,6 +14,12 @@ describe "Static pages" do
     	page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
     end
 
+    it "should not have a custom page title" do
+    	visit '/static_pages/home'
+    	page.should_not_have_selector('title', :text => '| Home')
+    end
+
+
   end
 
   describe "Help page" do
@@ -40,6 +46,21 @@ describe "Static pages" do
     it "should have the right title" do
     	visit '/static_pages/about'
     	page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
+    end
+
+  end
+
+
+  describe "Contact" do
+
+  	it "Should have the content of 'Contact'" do
+  		visit '/static_pages/contact'
+  		page.should have_content('Contact')
+  	end
+
+    it "should have the right title" do
+    	visit '/static_pages/contact'
+    	page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
     end
 
   end
